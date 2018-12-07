d277671ad77814cd273787dde4c4a6f923685d37
Johanna Larsson <johanna@castle.io>
2018-12-06 19:35:56 -0800

    added sinon fake timer and removed some double typing

--- test/lib.test.ts
+++ test/lib.test.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { Castle } from '../index';
 import fetchMock from 'fetch-mock';
 import { EVENTS } from '../src/events';
+import sinon from 'sinon';
 
 // event: string;
 // user_id: string;
@@ -22,12 +23,13 @@ describe('Castle', () => {
   describe('track', () => {
     it('should make a network request with some basic information', () => {
       const fetch = fetchMock.post('*', 204).sandbox();
+      const clock = sinon.useFakeTimers(new Date(2011, 9, 1).getTime());
 
       const castle = new Castle({
         apiSecret: 'some secret',
         overrideFetch: fetch,
       });
-      castle.track({
+      const requestData = {
         event: EVENTS.EMAIL_CHANGE_SUCCEEDED,
         user_id: 'userid',
         user_traits: {
@@ -41,24 +43,36 @@ describe('Castle', () => {
             cookie: 'SECRET=pleasedontbehere',
           },
         },
-      });
+      };
+      castle.track(requestData);
 
       const lastOptions = fetch.lastOptions();
       const payload = JSON.parse(lastOptions.body.toString());
       // Ensure the client set the sent_at property.
-      expect(payload).to.have.property('sent_at');
+      expect(payload).to.have.property('sent_at', new Date().toISOString());
       // Verify that the passed in properties are passed on.
-      expect(payload).to.have.property('event', EVENTS.EMAIL_CHANGE_SUCCEEDED);
-      expect(payload).to.have.property('user_id', 'userid');
+      expect(payload).to.have.property('event', requestData.event);
+      expect(payload).to.have.property('user_id', requestData.user_id);
       expect(payload).to.have.property('user_traits');
-      expect(payload.user_traits).to.have.property('email', 'myemail');
-      expect(payload.user_traits).to.have.property('registered_at', 'today');
+      expect(payload.user_traits).to.have.property(
+        'email',
+        requestData.user_traits.email
+      );
+      expect(payload.user_traits).to.have.property(
+        'registered_at',
+        requestData.user_traits.registered_at
+      );
       expect(payload).to.have.property('context');
-      expect(payload.context).to.have.property('ip', '8.8.8.8');
-      expect(payload.context).to.have.property('client_id', 'clientid');
+      expect(payload.context).to.have.property('ip', requestData.context.ip);
+      expect(payload.context).to.have.property(
+        'client_id',
+        requestData.context.client_id
+      );
       expect(payload.context).to.have.property('headers');
       // Ensure that cookie header property is scrubbed.
       expect(payload.context.headers).to.have.property('cookie', true);
+
+      clock.restore();
     });
   });
 });
