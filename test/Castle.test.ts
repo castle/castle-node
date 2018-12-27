d2b2692ec440791d136bb62bba8fb87d6f98486d
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2018-12-27 16:27:32 +0100

    fix: Correctly support failover and do not track (#12)
    Handle the case where our API responds with 500, where we should failover.
    
    Also handle the case where doNotTrack is set, where we should failover and not do outbound requests.

--- test/Castle.test.ts
+++ test/Castle.test.ts
@@ -162,6 +162,23 @@ describe('Castle', () => {
       expect(payload.context.headers).to.have.property('X-SUPER-SECRET', true);
     });
 
+    it('should not do requests when do not track is set', async () => {
+      const fetch = fetchMock.sandbox().post('*', 500);
+      const castle = new Castle({
+        apiSecret: 'some secret',
+        overrideFetch: fetch,
+        doNotTrack: true,
+        failoverStrategy: 'deny',
+      });
+
+      await castle.track(sampleRequestData);
+
+      // Ensure that fetch was never called. When do not track
+      // is on, the SDK should generate no outbound requests.
+      // tslint:disable-next-line:no-unused-expression
+      expect(fetch.called()).to.be.false;
+    });
+
     it('should fail on unauthorized', async () => {
       const fetch = fetchMock.sandbox().post('*', 401);
       const castle = new Castle({
@@ -255,6 +272,45 @@ describe('Castle', () => {
       expect(response).to.have.property('failover', true);
       expect(response).to.have.property('failover_reason', 'timeout');
       expect(response).to.have.property('user_id', 'userid');
+    });
+
+    it('should failover on 500', async () => {
+      const fetch = fetchMock.sandbox().post('*', 500);
+      const castle = new Castle({
+        apiSecret: 'some secret',
+        overrideFetch: fetch,
+        failoverStrategy: 'deny',
+        // This test causes an error level log event, so increase
+        // logLevel to fatal to prevent clouding the test output.
+        logLevel: 'fatal',
+      });
+
+      const response = await castle.authenticate(sampleRequestData);
+      expect(response).to.have.property('action', 'deny');
+      expect(response).to.have.property('failover', true);
+      expect(response).to.have.property('failover_reason', 'server error');
+      expect(response).to.have.property('user_id', 'userid');
+    });
+
+    it('should failover when do not track is set', async () => {
+      const fetch = fetchMock.sandbox().post('*', 500);
+      const castle = new Castle({
+        apiSecret: 'some secret',
+        overrideFetch: fetch,
+        doNotTrack: true,
+        failoverStrategy: 'deny',
+      });
+
+      const response = await castle.authenticate(sampleRequestData);
+      expect(response).to.have.property('action', 'deny');
+      expect(response).to.have.property('failover', true);
+      expect(response).to.have.property('failover_reason', 'do not track');
+      expect(response).to.have.property('user_id', 'userid');
+
+      // Ensure that fetch was never called. When do not track
+      // is on, the SDK should generate no outbound requests.
+      // tslint:disable-next-line:no-unused-expression
+      expect(fetch.called()).to.be.false;
     });
 
     it('should fail on unauthorized', async () => {
