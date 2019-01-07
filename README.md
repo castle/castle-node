70dd9ba2c1f1b96f4a4d0856ea6b723f899484b7
Johanna Larsson <johanna@castle.io>
2019-01-07 09:43:48 +0100

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,130 @@
+# Node SDK for Castle
+
+**[Castle](https://castle.io) analyzes device, location, and interaction patterns in your web and mobile apps and lets you stop account takeover attacks in real-time..**
+
+## Documentation
+
+[Official Castle docs](https://castle.io/docs)
+
+## Installation
+
+Add the `@castleio/sdk` package to your `package.json`.
+
+### yarn
+
+```bash
+yarn add @castleio/sdk
+```
+
+### npm
+
+```bash
+npm install --save @castleio/sdk
+```
+
+## Configuration
+
+### Framework configuration
+
+Load and configure the library with your Castle API secret in an initializer or similar.
+
+```js
+import { Castle } from '@castleio/sdk';
+
+const castle = Castle({ apiSecret: 'YOUR SECRET HERE' });
+```
+
+#### Config options
+
+| Config option     | Explanation                                                                                                                                                                             |
+| ----------------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| apiSecret         | `string` - This can be found in the castle dashboard.                                                                                                                                   |
+| timeout           | `number` - Time before returning the failover strategy. Default value is 500.                                                                                                           |
+| allowedHeaders    | `string[]` - An array of strings matching the headers you want to pass fully to the service.                                                                                            |
+| disallowedHeaders | `string[]` - An array of of strings matching the headers you do not want to pass fully to the service.                                                                                  |
+| failoverStrategy  | `string` - If the request to our service would for some reason time out, this is where you select the automatic response from `authenticate`. Options are `allow`, `deny`, `challenge`. |
+| logLevel          | `string` - Corresponds to standard log levels: `trace`, `debug`, `info`, `warn`, `error`, `fatal`. Useful levels are `info` and `error`.                                                |
+| doNotTrack        | `boolean` - False by default, setting it to true turns off all requests and triggers automatic failover on `authenticate`. Used for development and testing.                            |
+
+## Actions
+
+The `castle` instance exposes two methods, `track` and `authenticate`. In order to provide the information required in both these methods, you'll need access to the logged in user information (if that is available at that stage in the user flow), as well as request information. In node connect/express, the user is often found in the `session` object, or directly on the `request` object in the `user` property, if you are using passport.
+
+### track
+
+This is the asynchronous version of the castle integration. This is for events where you don't require a response.
+
+```js
+import { EVENTS } from '@castleio/sdk';
+
+track({
+  event: EVENTS.EMAIL_CHANGE_SUCCEEDED,
+  user_id: user.id,
+  user_traits: {
+    email: user.email,
+    registered_at: user.registered_at,
+  },
+  context: {
+    ip: request.ip,
+    client_id: request.cookies['__cid'],
+    headers: request.cookies,
+  },
+});
+```
+
+### authenticate
+
+This is the synchronous version of the castle integration. This is for events where you require a response. It is used in the same way as `track`, except that you have the option of waiting for a response.
+
+```js
+let response;
+try {
+  const response = await castle.authenticate({
+    event: EVENTS.EMAIL_CHANGE_SUCCEEDED,
+    user_id: user.id,
+    user_traits: {
+      email: user.email,
+      registered_at: user.registered_at,
+    },
+    context: {
+      ip: request.ip,
+      client_id: request.cookies['__cid'],
+      headers: request.headers,
+    },
+  });
+} catch (e) {
+  console.error(e);
+}
+
+console.log(response); // { "action": "allow", "user_id": 123, "device_token": "eyj...." }
+```
+
+####
+
+Response format
+
+| Response key    | value                                                                                          |
+| --------------- | ---------------------------------------------------------------------------------------------- |
+| action          | `string` - The recommended action for the given event. Options: `allow`, `challenge`, `deny`.  |
+| user_id         | `string` - The `user_id` of the end user.                                                      |
+| device_token    | `string` - Our token for the device that generated the event.                                  |
+| failover        | `boolean` - An optional property indicating the request failed and the response is a failover. |
+| failover_reason | `string` - A message indicating why the request failed.                                        |
+
+### All config options for `track` and `authenticate`
+
+| Config option | Explanation                                                                                                                                               |
+| ------------- | --------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| event         | `string` - The event generated by the user. It can be either an event from `EVENTS` or a custom one.                                                      |
+| user_id       | `string` - The `user_id` of the end user.                                                                                                                 |
+| user_traits   | `object` - An optional, recommended, object containing user information, such as `email` and `registered_at`.                                             |
+| properties    | `object` - An optional object containing custom information.                                                                                              |
+| created_at    | `string` - An optional ISO date string indicating when the event occurred, in cases where this might be different from the time when the request is made. |
+| device_token  | `string` - The optional device token, used for mitigating or escalating.                                                                                  |
+| context       | `object` - The request context information. See information below.                                                                                        |
+
+| Context option | Explanation                                                                                                                                                                                                      |
+| -------------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| ip             | `string` - The IP address of the request. Note that this needs to be the original request IP, not the IP of an internal proxy, such as nginx.                                                                    |
+| client_id      | `string` - The client ID, generated by the `c.js` integration on the front end. Commonly found in the `__cid` cookie in `request.cookies` or `request.cookie`, or in some cases the `X-CASTLE-CLIENT-ID` header. |
+| headers        | `object` - The headers object on the request, commonly `request.headers`.                                                                                                                                        |
---
+++ src/Castle.ts
@@ -0,0 +1,389 @@
+import fetch from 'node-fetch';
+import { reduce } from 'lodash';
+import { IncomingHttpHeaders } from 'http2';
+import AbortController from 'abort-controller';
+import packageJson from '../package.json';
+import pino from 'pino';
+
+const defaultApiUrl = 'https://api.castle.io';
+
+type CastleConstructorParameters = {
+  apiSecret: string;
+  apiUrl?: string;
+  timeout?: number;
+  allowedHeaders?: string[];
+  disallowedHeaders?: string[];
+  overrideFetch?: any;
+  failoverStrategy?: FailoverStrategyType;
+  logLevel?: pino.Level;
+  doNotTrack?: boolean;
+};
+
+type ActionParameters = {
+  event: string;
+  user_id: string;
+  user_traits?: object;
+  properties?: object;
+  created_at?: string;
+  device_token?: string;
+  context: {
+    ip: string;
+    client_id: string;
+    headers: IncomingHttpHeaders;
+  };
+};
+
+type ActionType = 'allow' | 'deny' | 'challenge';
+
+type FailoverStrategyType = ActionType | 'none';
+
+type AuthenticateResult = {
+  action: ActionType;
+  user_id?: string;
+  device_token?: string;
+  failover?: boolean;
+  failover_reason?: string;
+};
+
+type LoggingParameters = {
+  requestUrl: string;
+  requestOptions: any;
+  response?: Response;
+  err?: Error;
+  body?: any;
+};
+
+// The body on the request is a stream and can only be
+// read once, by default. This is a workaround so that the
+// logging functions can read the body independently
+// of the handlers.
+const getBody = async (response: any) => {
+  if (response.cachedBody) {
+    return response.cachedBody;
+  }
+
+  try {
+    response.cachedBody = await response.json();
+  } catch (e) {
+    response.cachedBody = {};
+  }
+
+  return response.cachedBody;
+};
+
+const isTimeout = (e: Error) => e.name === 'AbortError';
+
+const errorFormatter = (err: Error) =>
+  `
+Error name: ${err.name}
+Error message: ${err.message}
+`.trim();
+
+const responseFormatter = (response: Response, body: any) =>
+  `
+Response status: ${response.status}
+Response body: ${JSON.stringify(body)}
+`.trim();
+
+const requestFormatter = ({
+  requestUrl,
+  requestOptions,
+  response,
+  err,
+  body,
+}: LoggingParameters) => `
+-- Castle request
+URL: ${requestUrl}
+Request: ${JSON.stringify(requestOptions)}
+-- Castle response
+${response ? responseFormatter(response, body) : errorFormatter(err)}
+`;
+
+export class Castle {
+  private apiSecret: string;
+  private apiUrl: string;
+  private timeout: number;
+  private allowedHeaders: string[];
+  private disallowedHeaders: string[];
+  private overrideFetch: any;
+  private failoverStrategy: FailoverStrategyType;
+  private logger: pino.Logger;
+  private doNotTrack: boolean;
+
+  constructor({
+    apiSecret,
+    apiUrl,
+    timeout = 750,
+    allowedHeaders = [],
+    disallowedHeaders = [],
+    overrideFetch = fetch,
+    failoverStrategy = 'allow',
+    logLevel = 'error',
+    doNotTrack = false,
+  }: CastleConstructorParameters) {
+    if (!apiSecret) {
+      throw new Error(
+        'Castle: Unable to instantiate Castle client, API secret is missing.'
+      );
+    }
+
+    this.apiSecret = apiSecret;
+    this.apiUrl = apiUrl || defaultApiUrl;
+    this.timeout = timeout;
+    this.allowedHeaders = allowedHeaders.map(x => x.toLowerCase());
+    this.disallowedHeaders = disallowedHeaders
+      .concat(['cookie'])
+      .map(x => x.toLowerCase());
+    this.overrideFetch = overrideFetch;
+    this.failoverStrategy = failoverStrategy;
+    this.logger = pino({
+      prettyPrint: {
+        levelFirst: true,
+      },
+    });
+    this.logger.level = logLevel;
+    this.doNotTrack = doNotTrack;
+  }
+
+  public async authenticate(
+    params: ActionParameters
+  ): Promise<AuthenticateResult> {
+    if (!params.event) {
+      throw new Error('Castle: event is required when calling authenticate.');
+    }
+
+    if (this.doNotTrack) {
+      return this.generateFailoverBody(params, 'do not track');
+    }
+
+    let response: Response;
+    const controller = new AbortController();
+    const timeout = setTimeout(() => {
+      controller.abort();
+    }, this.timeout);
+    const requestUrl = `${this.apiUrl}/v1/authenticate`;
+    const requestOptions = {
+      signal: controller.signal,
+      method: 'POST',
+      headers: this.generateDefaultRequestHeaders(),
+      body: this.generateRequestBody(params),
+    };
+
+    try {
+      response = await this.getFetch()(requestUrl, requestOptions);
+    } catch (err) {
+      this.handleLogging({ requestUrl, requestOptions, err });
+
+      if (isTimeout(err)) {
+        return this.handleFailover(params, 'timeout', err);
+      } else {
+        throw err;
+      }
+    } finally {
+      clearTimeout(timeout);
+    }
+
+    // Wait to get body here to prevent race conditions
+    // on `.json()` because we attempt to read it in
+    // multiple places.
+    const body = await getBody(response);
+
+    this.handleLogging({ requestUrl, requestOptions, response });
+
+    if (response.status >= 500) {
+      return this.handleFailover(params, 'server error');
+    }
+
+    this.handleUnauthorized(response);
+    this.handleBadResponse(response);
+
+    return body;
+  }
+
+  public async track(params: ActionParameters): Promise<void> {
+    if (!params.event) {
+      throw new Error('Castle: event is required when calling track.');
+    }
+
+    if (this.doNotTrack) {
+      return;
+    }
+
+    let response: Response;
+    const controller = new AbortController();
+    const timeout = setTimeout(() => {
+      controller.abort();
+    }, this.timeout);
+    const requestUrl = `${this.apiUrl}/v1/track`;
+    const requestOptions = {
+      signal: controller.signal,
+      method: 'POST',
+      headers: this.generateDefaultRequestHeaders(),
+      body: this.generateRequestBody(params),
+    };
+
+    try {
+      response = await this.getFetch()(requestUrl, requestOptions);
+    } catch (err) {
+      if (isTimeout(err)) {
+        return this.handleLogging({ requestUrl, requestOptions, err });
+      }
+    } finally {
+      clearTimeout(timeout);
+    }
+
+    this.handleLogging({ requestUrl, requestOptions, response });
+    this.handleUnauthorized(response);
+    this.handleBadResponse(response);
+  }
+
+  private async handleLogging({
+    requestUrl,
+    requestOptions,
+    response,
+    err,
+  }: LoggingParameters) {
+    if (err) {
+      return this.logger.error(
+        requestFormatter({ requestUrl, requestOptions, err })
+      );
+    }
+
+    let body;
+    // Don't get body if status is NO CONTENT.
+    if (response.status !== 204) {
+      body = await getBody(response);
+    }
+
+    let log: pino.LogFn;
+
+    if (response.ok) {
+      log = this.logger.info.bind(this.logger);
+    } else if (response.status < 500 && response.status >= 400) {
+      log = this.logger.warn.bind(this.logger);
+    } else {
+      log = this.logger.error.bind(this.logger);
+    }
+
+    return log(
+      requestFormatter({ requestUrl, requestOptions, response, err, body })
+    );
+  }
+
+  private getFetch() {
+    return this.overrideFetch || fetch;
+  }
+
+  private scrubHeaders(headers: IncomingHttpHeaders) {
+    return reduce(
+      headers,
+      (accumulator: object, value: string, key: string) => {
+        if (this.disallowedHeaders.includes(key.toLowerCase())) {
+          return {
+            ...accumulator,
+            [key]: true,
+          };
+        }
+        if (
+          this.allowedHeaders.length &&
+          !this.allowedHeaders.includes(key.toLowerCase())
+        ) {
+          return {
+            ...accumulator,
+            [key]: true,
+          };
+        }
+
+        return {
+          ...accumulator,
+          [key]: value,
+        };
+      },
+      {}
+    );
+  }
+
+  private generateDefaultRequestHeaders() {
+    return {
+      Authorization: `Basic ${Buffer.from(`:${this.apiSecret}`).toString(
+        'base64'
+      )}`,
+      'Content-Type': 'application/json',
+    };
+  }
+
+  private generateRequestBody({
+    event,
+    user_id,
+    user_traits,
+    properties,
+    context,
+    created_at,
+    device_token,
+  }: ActionParameters) {
+    return JSON.stringify({
+      sent_at: new Date().toISOString(),
+      created_at,
+      event,
+      user_id,
+      user_traits,
+      properties,
+      device_token,
+      context: {
+        ...context,
+        client_id: context.client_id || false,
+        headers: this.scrubHeaders(context.headers),
+        library: {
+          name: 'castle-node',
+          version: packageJson.version,
+        },
+      },
+    });
+  }
+
+  private generateFailoverBody(
+    params: ActionParameters,
+    reason: string
+  ): AuthenticateResult {
+    return {
+      action:
+        // This is just for the type system, asurring it that failOverStrategy
+        // can not be 'none'.
+        this.failoverStrategy === 'none' ? 'allow' : this.failoverStrategy,
+      failover: true,
+      failover_reason: reason,
+      user_id: params.user_id,
+    };
+  }
+
+  private handleFailover(
+    params: ActionParameters,
+    reason: string,
+    err?: Error
+  ): AuthenticateResult {
+    // Have to check it this way to make sure TS understands
+    // that this.failoverStrategy is of type ActionType,
+    // not FailoverStrategyType.
+    if (this.failoverStrategy !== 'none') {
+      return this.generateFailoverBody(params, reason);
+    }
+
+    if (this.failoverStrategy === 'none') {
+      throw err;
+    }
+  }
+
+  private handleUnauthorized(response: Response) {
+    if (response.status === 401) {
+      throw new Error(
+        'Castle: Failed to authenticate with API, please verify the secret.'
+      );
+    }
+  }
+
+  private handleBadResponse(response: Response) {
+    if (response.status >= 400 && response.status < 500) {
+      throw new Error(`Castle: API response not ok, got ${response.status}.`);
+    }
+  }
+}
