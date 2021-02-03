e5dbd0ea4b510f1e38b3ab05cd6e2ee68c8e803d
marysieek <maria.korlotian@castle.io>
2021-02-03 17:40:38 +0100

    Remove events

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 export { Castle } from './src/Castle';
-export { EVENTS } from './src/events';
 export { DEFAULT_ALLOWLIST } from './src/constants';
 export { FailoverStrategy } from './src/failover/failover.module';
