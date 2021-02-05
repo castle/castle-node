06c4981cf332d73f575b300b5ba0ad37dbe49300
marysieek <maria.korlotian@castle.io>
2021-02-05 21:43:37 +0100

    Add response processing test

--- test/core/services/core-process-response.service.test.ts
+++ test/core/services/core-process-response.service.test.ts
@@ -1,29 +1,123 @@
 import { CoreProcessResponseService } from '../../../src/core/core.module';
-import { version } from '../../../package.json';
-import { Configuration } from '../../../src/configuraton';
-
 import pino from 'pino';
 import { Response } from 'node-fetch';
 
 describe('CoreProcessResponseService', () => {
   describe('call', () => {
-    describe('when success', () => {
-      const response = new Response(JSON.stringify({ user: 1 }), {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        status: 200,
+    describe('authenticate', () => {
+      describe('when success', () => {
+        const result = { user: 1 };
+        const response = new Response(JSON.stringify({ user: 1 }), {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          status: 200,
+        });
+
+        it('generates request body', async () => {
+          expect(
+            await CoreProcessResponseService.call(
+              'authenticate',
+              {},
+              response,
+              pino({ enabled: false })
+            )
+          ).toEqual(result);
+        });
       });
 
-      it('generates request body', async () => {
-        expect(
-          await CoreProcessResponseService.call(
-            'authenticate',
-            {},
-            response,
-            pino({ enabled: false })
-          )
-        ).toEqual({ user: 1 });
+      describe('when allow without additional props', () => {
+        const result = { action: 'allow', user_id: '12345' };
+        const response = new Response(JSON.stringify(result), {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          status: 200,
+        });
+
+        it('generates request body', async () => {
+          expect(
+            await CoreProcessResponseService.call(
+              'authenticate',
+              {},
+              response,
+              pino({ enabled: false })
+            )
+          ).toEqual(result);
+        });
+      });
+
+      describe('when allow with additional props', () => {
+        const result = { action: 'allow', user_id: '12345', internal: {} };
+        const response = new Response(JSON.stringify(result), {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          status: 200,
+        });
+
+        it('generates request body', async () => {
+          expect(
+            await CoreProcessResponseService.call(
+              'authenticate',
+              {},
+              response,
+              pino({ enabled: false })
+            )
+          ).toEqual(result);
+        });
+      });
+
+      describe('when deny without risk policy', () => {
+        const result = { action: 'deny', user_id: '1', device_token: 'abc' };
+        const response = new Response(JSON.stringify(result), {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          status: 200,
+        });
+
+        it('generates request body', async () => {
+          expect(
+            await CoreProcessResponseService.call(
+              'authenticate',
+              {},
+              response,
+              pino({ enabled: false })
+            )
+          ).toEqual(result);
+        });
+      });
+
+      describe('when deny with risk policy', () => {
+        const result = {
+          action: 'deny',
+          user_id: '1',
+          device_token: 'abc',
+          risk_policy: {
+            id: '123',
+            revision_id: 'abc',
+            name: 'def',
+            type: 'bot',
+          },
+        };
+        const response = new Response(JSON.stringify(result), {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          status: 200,
+        });
+
+        it('generates request body', async () => {
+          expect(
+            await CoreProcessResponseService.call(
+              'authenticate',
+              {},
+              response,
+              pino({ enabled: false })
+            )
+          ).toEqual(result);
+        });
       });
     });
   });
