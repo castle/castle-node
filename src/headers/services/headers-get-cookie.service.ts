d9839c436416c4bf9639cb7af81aa2b382e49a73
bartes <bartesrlz@gmail.com>
2023-05-04 11:21:35 +0200

    drop express js dependency

--- src/headers/services/headers-get-cookie.service.ts
+++ src/headers/services/headers-get-cookie.service.ts
@@ -1,11 +1,11 @@
 export const HeadersGetCookieService = {
-  call: (cookies: string, name: string): string | undefined => {
+  call: (cookies: string | string[], name: string): string | undefined => {
     if (!cookies) {
       return;
     }
 
     const pattern = new RegExp(`(?:^|\\s+)\\s?${name}=(.*?)(?:;|$)`);
-    const results = cookies.match(pattern);
+    const results = cookies.toString().match(pattern);
 
     if (results && results.length === 2) {
       // return the last match
