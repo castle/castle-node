030836955e4b0602c9993d2b09bcbb740f043b16
bartes <bartesrlz@gmail.com>
2023-05-04 12:44:53 +0200

    exposed List API

--- src/Castle.ts
+++ src/Castle.ts
@@ -10,6 +10,17 @@ import {
   APIFilterService,
   APIRiskService,
   APICreateListItemService,
+  APICreateListService,
+  APIUpdateListItemService,
+  APIUpdateListService,
+  APIArchiveListItemService,
+  APIUnarchiveListItemService,
+  APIDeleteListService,
+  APISearchListItemsService,
+  APISearchListsService,
+  APIFetchListItemService,
+  APIFetchListService,
+  APIFetchAllListsService,
 } from './api/api.module';
 import { FailoverStrategy } from './failover/failover.module';
 import type {
@@ -20,6 +31,13 @@ import type {
   DevicePayload,
   UserDevicePayload,
   CreateListItemPayload,
+  CreateListPayload,
+  UpdateListItemPayload,
+  UpdateListPayload,
+  ListItemPayload,
+  ListPayload,
+  SearchListItemsPayload,
+  SearchListsPayload,
 } from './payload/payload.module';
 import { Configuration, ConfigurationProperties } from './configuration';
 
@@ -46,7 +64,7 @@ export class Castle {
 
   public async risk(params: RiskPayload): Promise<object> {
     if (this.configuration.doNotTrack) {
-      return this.generateDoNotTrackResponse();
+      return this.generateDoNotTrackResponse(params.user.id);
     }
 
     return APIRiskService.call(params, this.configuration);
@@ -54,7 +72,7 @@ export class Castle {
 
   public async filter(params: FilterPayload): Promise<object> {
     if (this.configuration.doNotTrack) {
-      return this.generateDoNotTrackResponse();
+      return this.generateDoNotTrackResponse(params.matching_user_id);
     }
 
     return APIFilterService.call(params, this.configuration);
@@ -100,7 +118,51 @@ export class Castle {
     return APICreateListItemService.call(params, this.configuration);
   }
 
-  private generateDoNotTrackResponse(userId?): object {
+  public async createList(params: CreateListPayload): Promise<any> {
+    return APICreateListService.call(params, this.configuration);
+  }
+
+  public async updateListItem(params: UpdateListItemPayload): Promise<any> {
+    return APIUpdateListItemService.call(params, this.configuration);
+  }
+
+  public async updateList(params: UpdateListPayload): Promise<any> {
+    return APIUpdateListService.call(params, this.configuration);
+  }
+
+  public async archiveListItem(params: ListItemPayload): Promise<any> {
+    return APIArchiveListItemService.call(params, this.configuration);
+  }
+
+  public async unarchiveListItem(params: ListItemPayload): Promise<any> {
+    return APIUnarchiveListItemService.call(params, this.configuration);
+  }
+
+  public async deleteList(params: ListPayload): Promise<any> {
+    return APIDeleteListService.call(params, this.configuration);
+  }
+
+  public async fetchListItem(params: ListItemPayload): Promise<any> {
+    return APIFetchListItemService.call(params, this.configuration);
+  }
+
+  public async fetchList(params: ListPayload): Promise<any> {
+    return APIFetchListService.call(params, this.configuration);
+  }
+
+  public async fetchAllLists(): Promise<any> {
+    return APIFetchAllListsService.call(this.configuration);
+  }
+
+  public async searchListItems(params: SearchListItemsPayload): Promise<any> {
+    return APISearchListItemsService.call(params, this.configuration);
+  }
+
+  public async searchLists(params: SearchListsPayload): Promise<any> {
+    return APISearchListsService.call(params, this.configuration);
+  }
+
+  private generateDoNotTrackResponse(userId?): { [key: string]: any } {
     return {
       policy: {
         action: FailoverStrategy.allow,
