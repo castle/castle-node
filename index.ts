0377aaaa00b72ff155ba0d5747e10f4f56407f08
bartes <bartesrlz@gmail.com>
2023-05-06 11:56:04 +0200

    expose more declarations

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 export { Castle } from './src/Castle';
 export { ConfigurationProperties as CastleConfiguration } from './src/configuration';
 export * from './src/constants';
+export * from './src/models';
 export * from './src/errors';
-export { FailoverStrategy } from './src/failover/failover.module';
-export { PayloadPrepareService } from './src/payload/payload.module';
+export * from './src/failover/failover.module';
+export * from './src/payload/payload.module';
 export { ContextPrepareService } from './src/context/context.module';
