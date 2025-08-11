ab3aa6d8d0e5e89bb84fac71184062e1e79eb2df
Michał Knapik <mknapik@users.noreply.github.com>
2025-08-11 14:21:48 +0200

    fix: module resolution
    PRD-318

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { Castle } from './Castle';
+export { ConfigurationProperties as CastleConfiguration } from './configuration';
+export * from './constants';
+export * from './models';
+export * from './errors';
+export * from './failover/failover.module';
+export * from './payload/payload.module';
+export { ContextPrepareService } from './context/context.module';
